ff2b7351b1eed793d21595fd0b910f30349a30da
Daniel Gallichan <dgallichan@yahoo.com>
2022-05-23 14:03:53 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -10,9 +10,8 @@ basic.showLeds(`
     . . # # #
     `)
 radio.setGroup(99)
-radio.setTransmitSerialNumber(true)
 basic.forever(function () {
-    thisMessage = "" + control.deviceName() + "," + convertToText(input.acceleration(Dimension.X)) + "," + convertToText(input.acceleration(Dimension.Y)) + "," + convertToText(input.acceleration(Dimension.Z)) + "\n"
+    thisMessage = "#" + control.deviceName() + "," + convertToText(input.acceleration(Dimension.X)) + "," + convertToText(input.acceleration(Dimension.Y)) + "," + convertToText(input.acceleration(Dimension.Z)) + ";" + ""
     radio.sendString(thisMessage)
     led.toggle(4, 0)
     basic.pause(100)
