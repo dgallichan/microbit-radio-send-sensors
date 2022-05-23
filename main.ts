82d93894342c463efc3087d6eb688ac2ff1c69f0
Daniel Gallichan <dgallichan@yahoo.com>
2022-05-23 14:11:33 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,35 @@
 enum RadioMessage {
     message1 = 49434
 }
+input.onButtonPressed(Button.A, function () {
+    sendDelay += -10
+    if (sendDelay < 10) {
+        sendDelay = 10
+    }
+    basic.showNumber(sendDelay)
+    thisImage.showImage(0)
+})
+input.onButtonPressed(Button.B, function () {
+    sendDelay += 10
+    basic.showNumber(sendDelay)
+    thisImage.showImage(0)
+})
 let thisMessage = ""
-basic.showLeds(`
+let sendDelay = 0
+let thisImage: Image = null
+radio.setGroup(99)
+thisImage = images.createImage(`
     # # # . .
     . # . . .
     . # . . #
     . . . # #
     . . # # #
     `)
-radio.setGroup(99)
+sendDelay = 10
+thisImage.showImage(0)
 basic.forever(function () {
     thisMessage = "#" + control.deviceName() + "," + convertToText(input.acceleration(Dimension.X)) + "," + convertToText(input.acceleration(Dimension.Y)) + "," + convertToText(input.acceleration(Dimension.Z)) + ";" + ""
     radio.sendString(thisMessage)
     led.toggle(4, 0)
-    basic.pause(100)
+    basic.pause(sendDelay)
 })
