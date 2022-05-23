fd722e48c34df53f72d6aa2c4ab413a143b0ce58
Daniel Gallichan <dgallichan@yahoo.com>
2022-05-23 13:58:09 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+enum RadioMessage {
+    message1 = 49434
+}
 let thisMessage = ""
 basic.showLeds(`
     # # # . .
@@ -9,7 +12,7 @@ basic.showLeds(`
 radio.setGroup(99)
 radio.setTransmitSerialNumber(true)
 basic.forever(function () {
-    thisMessage = "" + control.deviceName() + "," + convertToText(input.acceleration(Dimension.X)) + "," + convertToText(input.acceleration(Dimension.Y)) + "," + convertToText(input.acceleration(Dimension.Z))
+    thisMessage = "" + control.deviceName() + "," + convertToText(input.acceleration(Dimension.X)) + "," + convertToText(input.acceleration(Dimension.Y)) + "," + convertToText(input.acceleration(Dimension.Z)) + "\n"
     radio.sendString(thisMessage)
     led.toggle(4, 0)
     basic.pause(100)
