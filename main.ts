4a02407fa9e26e8d8cc5dad2de52282da2a9be14
Daniel Gallichan <dgallichan@yahoo.com>
2022-05-23 16:06:44 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,10 +7,21 @@ input.onButtonPressed(Button.A, function () {
         sendDelay = 10
     }
 })
+input.onButtonPressed(Button.AB, function () {
+    trackMode += 1
+    if (trackMode > 1) {
+        trackMode = 0
+    }
+    for (let index = 0; index <= 4; index++) {
+        led.unplot(index, 4)
+    }
+    led.plot(trackMode, 4)
+})
 input.onButtonPressed(Button.B, function () {
     sendDelay += 50
 })
 let thisMessage = ""
+let trackMode = 0
 let sendDelay = 0
 radio.setGroup(99)
 let thisImage = images.createImage(`
@@ -18,12 +29,17 @@ let thisImage = images.createImage(`
     . # . . .
     . # . . #
     . . . # #
-    . . # # #
+    . . . . .
     `)
 sendDelay = 10
 thisImage.showImage(0)
+trackMode = 0
 basic.forever(function () {
-    thisMessage = "#" + control.deviceName() + "," + convertToText(input.acceleration(Dimension.X)) + "," + convertToText(input.acceleration(Dimension.Y)) + "," + convertToText(input.acceleration(Dimension.Z)) + ";" + ""
+    if (trackMode == 0) {
+        thisMessage = "#" + control.deviceName() + "," + convertToText(input.acceleration(Dimension.X)) + "," + convertToText(input.acceleration(Dimension.Y)) + "," + convertToText(input.acceleration(Dimension.Z)) + ";" + ""
+    } else {
+        thisMessage = "#" + control.deviceName() + "," + convertToText(input.rotation(Rotation.Pitch)) + "," + convertToText(input.rotation(Rotation.Roll)) + "," + convertToText(input.compassHeading()) + ";" + ""
+    }
     radio.sendString(thisMessage)
     led.toggle(4, 0)
     basic.pause(sendDelay)
